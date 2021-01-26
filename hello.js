99c213b1cbdc3cdd40e043380844df9032d95450
abuzar-gifari <shaikh35-3024@diu.edu.bd>
2021-01-26 21:11:04 +0600

    first commit

---
+++ hello.js
@@ -0,0 +1,20 @@
+var myfunc = function(){
+    return "Hello";
+}
+
+console.log(myfunc());
+
+/*
+Arrow function:
+1) var variable_name = () => {
+    function body
+}
+2) Arrow functions can not be used as constructors
+*/
+
+
+var MyArrow = (x,y,...u) => {
+    var z = x + y;
+    return z;
+}
+console.log(MyArrow(10,23,56,3,4,5,6,7,8,9,2));
